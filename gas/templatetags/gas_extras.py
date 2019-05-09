7a4d74e55aff3f4a3dfcf2314328378a9e9d6470
Kyle Laker <kyle@laker.email>
2019-05-09 09:36:48 -0400

    Track active page in navbar

---
+++ gas/templatetags/gas_extras.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def nav_active(context, item):
+    """
+    Returns 'active' is item is the active nav bar item, and '' otherwise.
+    """
+
+    default = ''
+    try:
+        url_name = context['request'].resolver_match.url_name
+    except:
+        return default
+
+    if url_name == item:
+        return 'active'
+    
+    return default
