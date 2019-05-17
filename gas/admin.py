8bc5b49308501881da51f0d1466daca5e7fdff37
Kyle Laker <kyle@laker.email>
2019-05-17 14:50:49 -0400

    Add better admin support for models

--- gas/admin.py
+++ gas/admin.py
@@ -3,8 +3,51 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import *
 
-# Register your models here.
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid', 'make', 'model', 'year', 'purchase_date', 'vin', 'owner'
+    )
+    list_display_links = (
+        'uuid', 'vin'
+    )
+    search_fields = (
+        'uuid', 'vin'
+    )
+
+    list_per_page = 25
+
+
+@admin.register(GasPurchase)
+class GasPurchaseAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid', 'datetime', 'gallons', 'cost_per_gallon', 'odometer_reading',
+        'total_cost', 'vehicle'
+    )
+    list_display_links = (
+        'uuid',
+    )
+    search_fields = (
+        'uuid', 'odometer_reading'
+    )
+
+    list_per_page = 25
+
+
+@admin.register(Maintenance)
+class MaintenanceAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid', 'datetime', 'vehicle', 'odometer_reading', 'cost'
+    )
+    list_display_links = (
+        'uuid',
+    )
+    search_fields = (
+        'uuid', 'odometer_reading'
+    )
+
+    list_per_page = 25
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Car)
-admin.site.register(GasPurchase)
-admin.site.register(Maintenance)
